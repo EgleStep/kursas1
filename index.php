d80b2e27f4feb9461314b963ab33318f54801f56
EgleStep <48230130+EgleStep@users.noreply.github.com>
2019-03-28 18:21:17 +0200

    uzduotis 5

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
 <?php
-    $cities = ['Berlynas', 'Roma', 'Londonas'];
-    $cities[ ] = 'Tokijas';
-
-    print_r($cities);
-
-    $cities2 = [
-    'tokijas' => 13.6,
-    'vasingtonas' => 0.6,
-    'maskva' => 11.5,
+    $cities3 = [
+    'Tokijas' => [13.6, 1868, 'Japonija'], 
+    'Vasingtonas' => [0.6, 1790, 'JAV'], 
+    'Maskva' => [11.5, 1147, 'Rusija'],
+    'Londonas' => [8.6, 43, 'Anglija']
     ];
-
-    $cities2[ ] = [
-    'londonas' => 8.6,
-    ];
+    
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Uzduotis 1 | PHP 2</title>
+    <title>Uzduotis 2 | PHP 2</title>
 </head>
 
 <body>
     <ul>
         <?php
-            echo "<li>$cities[1]</li>";
-            echo "<li>Gyventojų skaičius: $cities2[tokijas] mln.</li>";
+            echo "<li>Gyventojų skaičius: {$cities3['Londonas'][0]} mln.</li>";
+            echo "<li>Įkurtas: {$cities3['Londonas'][1]} m.</li>";
+            echo "<li>Šalis: {$cities3['Londonas'][2]}</li>";
+
         ?>
     </ul>
 </body>
